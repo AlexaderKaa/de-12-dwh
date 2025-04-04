95d92f270eeb9f11c1eee9666e81bca9406e4a07
aleksandr-k-vt <aleksandr-k-vt@yandex.ru>
2025-04-04 22:21:05 +0300

    Add deliveries

---
+++ Scripts/04_create_stg_schema_delivery_system_deliveries-01.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS stg.delivery_system_deliveries (
+	id 			int4 GENERATED ALWAYS AS IDENTITY NOT NULL,
+    order_id 	TEXT NOT NULL,
+    order_ts 	TEXT NOT NULL,
+    delivery_id TEXT NOT NULL,
+    courier_id 	TEXT NOT NULL,
+    address 	TEXT NOT NULL,
+    delivery_ts TEXT NOT NULL,
+    rate 		TEXT NOT NULL,	--INT CHECK (rate >= 1 AND rate <= 5),
+    sum 		TEXT NOT NULL, 	--DECIMAL(10, 2) NOT NULL,
+    tip_sum 	TEXT NOT NULL,	--DECIMAL(10, 2) NOT NULL,
+	CONSTRAINT delivery_system_delivery_delivery_id UNIQUE (delivery_id),
+	CONSTRAINT delivery_system_delivery_pkey PRIMARY KEY (id)
+);
+
+
+SELECT *  FROM stg.delivery_system_deliveries;
+
+/*
+{'order_id': '67c470301c73132b9d05fff8', 'order_ts': '2025-03-02 14:50:24.637000', 'delivery_id': 'nkqvdql4hg6pos145xf3t0c', 'courier_id': 'lzxrn5kjsn493wjp2pxq05m',
+'address': 'Ул. Старая, 6, кв. 467', 'delivery_ts': '2025-03-02 15:57:33.704000', 'rate': 5, 'sum': 840, 'tip_sum': 84}
+*/
+
